c016442eb0658cfa4539aabef1639a9f8b6e3ab2
Drew Lazzeri <drew@interviewing.io>
2018-09-02 02:55:51 -0700

    Fuckin dates parsing from google and printing okay

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate hyper;
 extern crate hyper_rustls;
 extern crate job_scheduler;
 extern crate yup_oauth2 as oauth2;
+extern crate itertools;
 
 use job_scheduler::{Job, JobScheduler};
 
@@ -11,11 +12,14 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::time::Duration;
+use itertools::Itertools;
 
-use calendar3::CalendarHub;
-use calendar3::Error;
-use chrono::prelude::Local;
+use calendar3::{CalendarHub, Error, Event};
+use chrono::prelude::{Local};
 use chrono::Duration as OlderDuration; //recommended nameing in docs, i think
+use chrono::{DateTime, NaiveDate, NaiveTime, NaiveDateTime};
+use chrono::Datelike;
+use chrono::offset::*;
 use oauth2::{
     read_application_secret, ApplicationSecret, Authenticator, DefaultAuthenticatorDelegate,
     MemoryStorage,
@@ -24,7 +28,7 @@ use std::default::Default;
 
 fn main() {
     let secret: ApplicationSecret =
-        read_application_secret(&Path::new("./secret")).expect("Couldn't read it");
+        read_application_secret(&Path::new("./secret")).expect("Couldn't read application secret");
     // Instantiate the authenticator. It will choose a suitable authentication flow for you,
     // unless you replace  `None` with the desired Flow.
     // Provide your own `AuthenticatorDelegate` to adjust the way it operates and get feedback about
@@ -46,18 +50,18 @@ fn main() {
         auth,
     );
 
-    let path = Path::new("/dev/serial0");
+    let path = Path::new("./output");
     let display = path.display();
 
     // Open a file in write-only mode, returns `io::Result<File>`
     let mut file = match File::create(&path) {
-        Err(why) => panic!("couldn't create file in write-only mode"),
+        Err(why) => panic!("couldn't create file in write-only mode: {}", why),
         Ok(file) => file,
     };
 
     let mut sched = JobScheduler::new();
 
-    sched.add(Job::new("0 0 8 * * *".parse().unwrap(), || {
+//    sched.add(Job::new("0 * * * * *".parse().unwrap(), || {
         let now = Local::now();
         let next_week = now
             .clone()
@@ -78,37 +82,62 @@ fn main() {
                 // The Error enum provides details about what exactly happened.
                 // You can also just use its `Debug`, `Display` or `Error` traits
                 Error::HttpError(_)
-                | Error::MissingAPIKey
-                | Error::MissingToken(_)
-                | Error::Cancelled
-                | Error::UploadSizeLimitExceeded(_, _)
-                | Error::Failure(_)
-                | Error::BadRequest(_)
-                | Error::FieldClash(_)
-                | Error::JsonDecodeError(_, _) => println!("{}", e),
+                    | Error::MissingAPIKey
+                    | Error::MissingToken(_)
+                    | Error::Cancelled
+                    | Error::UploadSizeLimitExceeded(_, _)
+                    | Error::Failure(_)
+                    | Error::BadRequest(_)
+                    | Error::FieldClash(_)
+                    | Error::JsonDecodeError(_, _) => println!("{}", e),
             },
             Ok((_res, events)) => {
-                let items = events.items.expect("No items to parse");
-                for item in items.into_iter() {
-                    // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
-                    let string = format!(
-                        "\n\n{:?} starts at {:?}\n\n",
-                        item.summary.expect("No summary for event"),
-                        item.start.expect("No start time for event")
-                    );
-                    match file.write_all(&string.as_bytes()) {
-                        Err(why) => panic!("couldn't write to printer"),
-                        Ok(_) => println!("successfully wrote to {}", display),
-                    }
-                    println!();
+                println!("{:?}", events);
+                let string = string_from_items(events.items.expect("No items to parse"));
+                match file.write_all(&string.as_bytes()) {
+                    Err(why) => panic!("couldn't write to printer: {}", why),
+                    Ok(_) => println!("successfully wrote to {}", display),
                 }
             }
         }
-    }));
+//    }));
 
     loop {
         sched.tick();
 
         std::thread::sleep(Duration::from_millis(500));
     }
+}
+
+fn string_from_items(items: Vec<Event>) -> std::string::String {
+    let mut return_string: std::string::String = "".to_string();
+    let simplified_events= items.into_iter().map(|event| {
+        let start = event.start.expect("No start time for event");
+        //TODO I really wanna break out this date logic, but I can't think of what the type interface would be
+        let when = match start.date {
+            Some(time) => {let date = NaiveDate::parse_from_str(&time, "%Y-%m-%d").expect("Couldn't parse into Naive Date");
+                           let time = NaiveTime::from_hms(0,0,0);
+                           let date_time = NaiveDateTime::new(date, time);
+                           FixedOffset::west(7 * 3600).from_local_datetime(&date_time).unwrap()
+            },
+            None => match start.date_time {
+                Some(time) => DateTime::parse_from_rfc3339(&time).expect("Couldn't parse dates"),
+                None => panic!("There isn't a date or a date_time on event {:?}", start),
+            },
+        };
+        let summary = event.summary.expect("No summary for event");
+        (when,  summary)
+    });
+
+    //TODO: Recurring events have dates from when they were started :/
+    let sorted_events = simplified_events.sorted_by_key(|t| t.0);
+
+    for (key, group) in &sorted_events.into_iter().group_by(|t| t.0){ 
+        //It seems x.group_by().length == x.length, how do I group??
+        return_string.push_str(&format!("{:?}:", key.weekday()));
+        for event in group.into_iter() {
+            return_string.push_str(&event.1);
+        }
+    }
+    return_string
 }
