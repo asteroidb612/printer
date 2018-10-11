d1054303627043a1d7655a170c78cc23d820fcf0
Drew Lazzeri <drew@interviewing.io>
2018-10-11 01:35:46 -0700

    Added ynab!

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,15 @@ extern crate hyper;
 extern crate hyper_rustls;
 extern crate itertools;
 extern crate job_scheduler;
+extern crate reqwest;
 extern crate yup_oauth2 as oauth2;
 
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
-#[macro_use] extern crate rouille;
+#[macro_use]
+extern crate rouille;
 
 use job_scheduler::{Job, JobScheduler};
 
@@ -64,7 +67,8 @@ fn main() {
     let hub2 = hub.clone(); //Appease borrow checking gods
 
     // Open a file in write-only mode, returns `io::Result<File>`
-    let mut printer = match File::create(Path::new(DEFAULT_PATH)) { //I have a lot of good case studies in here for moving/borrowing... this is one...
+    let mut printer = match File::create(Path::new(DEFAULT_PATH)) {
+        //I have a lot of good case studies in here for moving/borrowing... this is one...
         Err(why) => panic!("couldn't create file in write-only mode: {}", why),
         Ok(file) => file,
     };
@@ -74,23 +78,24 @@ fn main() {
     let mut cron = JobScheduler::new();
     let path = Path::new("store.json");
     let display = path.display();
-    let model = Model {games: match File::open(&path) {
-        Err(_why) => vec![],
-        Ok(mut file) => {
-            let mut s = String::new();
-            match file.read_to_string(&mut s) {
-                Err(_why) => vec![],
-                Ok(_) =>  {
-                    match serde_json::from_str(&mut s) {
+    let model = Model {
+        games: match File::open(&path) {
+            Err(_why) => vec![],
+            Ok(mut file) => {
+                let mut s = String::new();
+                match file.read_to_string(&mut s) {
+                    Err(_why) => vec![],
+                    Ok(_) => match serde_json::from_str(&mut s) {
                         Err(_why) => vec![],
-                        Ok(parsed_store) => parsed_store
-                    }
+                        Ok(parsed_store) => parsed_store,
+                    },
                 }
             }
-        }
-    }};
+        },
+    };
     let share_for_web_interface = Arc::new(Mutex::new(model)); //I guess haveing two of these means moving's fine
     let share_for_cron = share_for_web_interface.clone(); //What are memory implications of a move?
+    let share_for_ynab = share_for_web_interface.clone();
 
     let ping_server = || {
         let now = Local::now();
@@ -145,7 +150,7 @@ fn main() {
                     println!("{}", e);
                     let message = "\n\nUnable to connect to Google ";
                     printer.write_all(&message.as_bytes()).unwrap();
-                },
+                }
             },
             Ok((_res, events)) => {
                 let u = share_for_cron.lock().unwrap();
@@ -167,6 +172,32 @@ fn main() {
             }
         }
     };
+
+    let check_ynab_api =
+        move || {
+            let client = reqwest::Client::new();
+            let url = "";
+            let bear = ""; //move to file
+            let mut res = client
+                .get(url)
+                .bearer_auth(bear)
+                .send()
+                .expect("Couldn't send request to YNAB");
+
+            let response: YNABResponse = res.json().expect("Couldn't parse respone from YNAB");
+            if response.data.transactions.into_iter().all(|transaction| {
+                match transaction.flag_color {
+                    None => transaction.approved,
+                    _ => true,
+                }
+            }) {
+                let mut model = share_for_ynab.lock().unwrap();
+                *model = updated(
+                    &mut *model,
+                    Msg::GameOccurence("ynab".to_owned(), Local::now()),
+                );
+            }
+        };
 
     std::thread::spawn(|| {
         rouille::start_server("0.0.0.0:3030", move |request| {
@@ -207,18 +238,18 @@ fn main() {
                     Err(_) => panic!("server couldn't write store to file"),
                     Ok(_) => ()
                 };
-
-                Response::text(serialized)                
+                Response::text(serialized)
             })
         });
     });
 
     ping_server();
-    cron.add(Job::new("0 30 * * * * *".parse().unwrap(), ping_server));
+    cron.add(Job::new("0 30 * * * *".parse().unwrap(), ping_server));
     cron.add(Job::new(
-        "0 0 15 * * * *".parse().unwrap(),
+        "0 0 15 * * *".parse().unwrap(), //Package users Greenwhich mean time, so PAC is 15 - 7 == 8:00
         print_next_five_days,
     ));
+    cron.add(Job::new("0 0 /3 0 0 0".parse().unwrap(), check_ynab_api)); //Hours divisible by 3
     loop {
         cron.tick();
 
@@ -295,7 +326,11 @@ fn weekday_name(w: Weekday) -> std::string::String {
 //}
 
 fn github_graph(g: &Game) -> String {
-    let dates = &g.events.iter().map(|l| DateTime::date(&l.when)).collect::<Vec<Date<Local>>>();
+    let dates = &g
+        .events
+        .iter()
+        .map(|l| DateTime::date(&l.when))
+        .collect::<Vec<Date<Local>>>();
     let today = Local::now().date();
     let min = dates.iter().min().expect("No dates so far").clone(); //TODO why does clone() change the type here? //(Later) do I see a type error or a borrow error...
     let mut date = min.clone();
@@ -318,14 +353,17 @@ fn github_graph(g: &Game) -> String {
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-struct Event {what: String, when: chrono::DateTime<Local>}
+struct Event {
+    what: String,
+    when: chrono::DateTime<Local>,
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-struct Game  {
+struct Game {
     name: String,
     //start: chrono::DateTime<Local>,
     //end: chrono::DateTime<Local>,
-    events: Vec<Event>
+    events: Vec<Event>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -334,22 +372,44 @@ struct Model {
 }
 
 enum Msg {
-    GameOccurence(String, chrono::DateTime<Local>)
+    GameOccurence(String, chrono::DateTime<Local>),
 }
-    
 
-fn updated(model: & mut Model, msg: Msg) -> Model {
+#[derive(Deserialize, Debug)]
+struct YNABResponse {
+    data: Data,
+}
+
+#[derive(Deserialize, Debug)]
+struct Data {
+    transactions: Vec<Transaction>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Transaction {
+    approved: bool,
+    flag_color: Option<String>,
+}
+
+fn updated(model: &mut Model, msg: Msg) -> Model {
     let c = model.clone(); //Really? I Have to borrow mut AND clone? Could I just clone? What problems is each solving??
     match msg {
-        Msg::GameOccurence(game, time) => Model {games:
-           c.games.into_iter().map(|mut stored_game| {
-               if stored_game.name == game {
-                   stored_game.events.push(Event {what:game.clone(), when:time});
-                   stored_game //Hey that's not immutable... maybe I miss conslists
-               } else {
-                   stored_game
-               }
-           }).collect()
-       }
-   }
+        Msg::GameOccurence(game, time) => Model {
+            games: c
+                .games
+                .into_iter()
+                .map(|mut stored_game| {
+                    if stored_game.name == game {
+                        stored_game.events.push(Event {
+                            what: game.clone(),
+                            when: time,
+                        });
+                        stored_game //Hey that's not immutable... maybe I miss conslists
+                    } else {
+                        stored_game
+                    }
+                })
+                .collect(),
+        },
+    }
 }
