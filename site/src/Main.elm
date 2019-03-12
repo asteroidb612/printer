547f76655239f09709b7ee169612aea33e533a76
Drew <dlazzeri1@gmail.com>
2019-03-12 13:26:27 -0700

    Resolved dependency issue and compiled minimal, untested decoder

--- site/src/Main.elm
+++ site/src/Main.elm
@@ -1,10 +1,12 @@
 module Main exposing (main)
 
 import Browser exposing (document)
-import Dict exposing (Dict)
+import Debug
 import Html exposing (a, div, text)
 import Html.Attributes exposing (href)
+import Http
 import Iso8601
+import Json.Decode as D
 import List exposing (map)
 import Time exposing (Posix)
 
@@ -28,11 +30,12 @@ type alias Game =
 
 
 type alias Model =
-    { games : Maybe (Dict String Game) }
+    { games : Maybe (List Game) }
 
 
 type Msg
-    = NoOp
+    = GotGames (Result Http.Error (List Game))
+    | ButtonPressed
 
 
 view model =
@@ -43,5 +46,28 @@ view model =
 
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        GotGames result ->
+            case result of
+                Ok games ->
+                    ( { model | games = Just games }, Cmd.none )
+
+                _ ->
+                    Debug.todo "What should I do if I can't parse games from the server?"
+
+        ButtonPressed ->
+            ( model, fetchGames )
+
+
+fetchGames =
+    Http.get
+        { url = "https://de46adb5aefddd002ff3c4227d43b588.balena-devices.com/read_game_file"
+        , expect = Http.expectJson GotGames (D.list decodeGame)
+        }
+
+
+decodeGame =
+    D.map4 Game
+        (D.field "name" D.string)
+        (D.field "start" Iso8601.decoder)
+        (D.field "end" Iso8601.decoder)
+        (D.field "events" (D.list Iso8601.decoder))
