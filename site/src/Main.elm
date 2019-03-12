473f89c5ade94eb76de358d803f59f5788379549
Drew <dlazzeri1@gmail.com>
2019-03-11 23:29:57 -0700

    Set elm up nicer

--- site/src/Main.elm
+++ site/src/Main.elm
@@ -1,10 +1,47 @@
 module Main exposing (main)
 
+import Browser exposing (document)
+import Dict exposing (Dict)
 import Html exposing (a, div, text)
 import Html.Attributes exposing (href)
+import Iso8601
 import List exposing (map)
+import Time exposing (Posix)
 
-games = ["wake and code"]
-main = div [] <| List.map link_from_game games
 
-link_from_game game_name = a [href game_name] [text game_name]
+main =
+    document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+init : () -> ( Model, Cmd msg )
+init flags =
+    ( { games = Nothing }, Cmd.none )
+
+
+type alias Game =
+    { name : String, start : Posix, end : Posix, events : List Posix }
+
+
+type alias Model =
+    { games : Maybe (Dict String Game) }
+
+
+type Msg
+    = NoOp
+
+
+view model =
+    { title = "Ludi"
+    , body = [ text "Hello World" ]
+    }
+
+
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
