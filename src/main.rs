4655645f9f7fdca1c3e4e786584989d2365479b3
Drew <dlazzeri1@gmail.com>
2019-07-07 22:17:08 -0700

    Add poem

--- src/main.rs
+++ src/main.rs
@@ -279,6 +279,23 @@ fn main() {
                         //}
                     }
                 }
+        print(format!("Gerard Manley Hopkins (1844–89).  Poems.  1918.
+ 
+13. Pied Beauty
+ 
+ 
+GLORY be to God for dappled things—	
+  For skies of couple-colour as a brinded cow;	
+    For rose-moles all in stipple upon trout that swim;	
+Fresh-firecoal chestnut-falls; finches' wings;	
+  Landscape plotted and pieced—fold, fallow, and plough;	        5
+    And áll trádes, their gear and tackle and trim.	
+ 
+All things counter, original, spare, strange;	
+  Whatever is fickle, freckled (who knows how?)	
+    With swift, slow; sweet, sour; adazzle, dim;	
+He fathers-forth whose beauty is past change:	        10
+                  Praise him."));
     };
     let _check_ynab_api =
         move || {
