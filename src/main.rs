b6128318c84b54311d3c55a01cf97731cf80ba24
Drew <dlazzeri1@gmail.com>
2019-07-28 23:24:59 -0700

    Revert "Change printing for debug on balena"
    This reverts commit 9f039835e8b2533e85b0e92a4522d9a03e2e0a1d.

--- src/main.rs
+++ src/main.rs
@@ -51,8 +51,7 @@ cfg_if! {
         static STORAGE: &str = "store.json";
         static TOKEN_STORAGE: &str = "token";
     } else {
-        static PRINTER_PATH: &str = "/dev/stdout";
-        //static PRINTER_PATH: &str = "/dev/serial0";
+        static PRINTER_PATH: &str = "/dev/serial0";
         static PORT: &str = "0.0.0.0:80";
         static STORAGE: &str = "/data/store.json";
         static TOKEN_STORAGE: &str = "/data/token";
